3dd1a0c1b9ef1e890fdfdad2db4183c3ccad3a13
Naohisa Goto <ng@bioruby.org>
2015-09-16 17:16:27 +0900

    Stub files in for smooth migration from BioRuby
     * lib/bio/db/phyloxml/phyloxml_elements.rb, phyloxml_parser.rb,
       phyloxml_writer.rb: For keeping compatibility with old programs
       that use "require 'bio/db/phyloxml/phyloxml_elements'" etc.
     * lib/bio-phyloxml/compat/stub_phyloxml_elements.rb,
       stub_phyloxml_parser.rb, stub_phyloxml_writer.rb: stub files
       for preventing circular require.

---
+++ lib/bio/db/phyloxml/phyloxml_elements.rb
@@ -0,0 +1,4 @@
+# for migration from old bioruby to bio-phyloxml
+if require 'bio-phyloxml/compat/stub_phyloxml_elements' then
+  require 'bio-phyloxml'
+end
---
+++ lib/bio/db/phyloxml/phyloxml_writer.rb
@@ -0,0 +1,4 @@
+# for migration from old bioruby to bio-phyloxml
+if require 'bio-phyloxml/compat/stub_phyloxml_writer' then
+  require 'bio-phyloxml'
+end
