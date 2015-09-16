884ad455dab7cff6cbf829d64cb0ee0adb2b3530
Naohisa Goto <ng@bioruby.org>
2015-09-16 17:59:05 +0900

    load bio-phyloxml/compat/cleanup.rb and stub files for migration

--- lib/bio-phyloxml.rb
+++ lib/bio-phyloxml.rb
@@ -8,7 +8,20 @@
 #
 # In this file only require other files. Avoid other source code.
 
+require 'bio-phyloxml/compat/cleanup.rb'
 require 'bio-phyloxml/version.rb'
 require 'bio-phyloxml/phyloxml_elements.rb'
 require 'bio-phyloxml/phyloxml_parser.rb'
 require 'bio-phyloxml/phyloxml_writer.rb'
+
+if require 'bio-phyloxml/compat/stub_phyloxml_elements.rb'
+  require_relative 'bio/db/phyloxml/phyloxml_elements.rb'
+end
+
+if require 'bio-phyloxml/compat/stub_phyloxml_parser.rb'
+  require_relative 'bio/db/phyloxml/phyloxml_parser.rb'
+end
+
+if require 'bio-phyloxml/compat/stub_phyloxml_writer.rb'
+  require_relative 'bio/db/phyloxml/phyloxml_writer.rb'
+end
