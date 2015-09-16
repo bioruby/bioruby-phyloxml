86bd6a3bdad1c42bba6e62c5be4c3cf7c8bced6e
Naohisa Goto <ng@bioruby.org>
2015-09-16 16:39:12 +0900

    add lib/bio-phyloxml.rb to load Bio::PhyloXML

---
+++ lib/bio-phyloxml.rb
@@ -0,0 +1,14 @@
+# Please require your code below, respecting the naming conventions in the
+# bioruby directory tree.
+#
+# For example, say you have a plugin named bio-plugin, the only uncommented
+# line in this file would be
+#
+#   require 'bio/bio-plugin/plugin'
+#
+# In this file only require other files. Avoid other source code.
+
+require 'bio-phyloxml/version.rb'
+require 'bio-phyloxml/phyloxml_elements.rb'
+require 'bio-phyloxml/phyloxml_parser.rb'
+require 'bio-phyloxml/phyloxml_writer.rb'
